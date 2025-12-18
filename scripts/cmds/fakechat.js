b3af045877e980907943e04ad617a68f05fafd94
Stærboy Âphelion  (Gangstar ß) <chistytsu@gmail.com>
2025-12-18 14:14:07 +0600

    Update fakechat.js

--- scripts/cmds/fakechat.js
+++ scripts/cmds/fakechat.js
@@ -1,62 +1,115 @@
-const axios = require("axios");
 
 module.exports = {
-  config: {
-    name: "fakechat",
-    aliases: ["fc"],
-    version: "1.0",
-    author: "♡︎ 𝐻𝐴𝑆𝐴𝑁 ♡︎",
-    countDown: 5,
-    role: 0,
-    shortDescription: "fakechat",
-    longDescription: "generate fakechat",
-    category: "fun",
-    guide: {
-      en: "{pn} @tag or reply to a message"
+	config: {
+		name: "fakechat",
+		aliases: ["q", "gc"],
+		author: "Tawsif~",
+		category: "fun",
+		version: "2.5 pro",
+		countDown: 5,
+		role: 0,
+		shortDescription: "create fakechat image",
+		guide: {
+			en: "<text> ++ <text> | reply | --own <texts> | --user <uid> | --attachment <image url> | --time <true or false> | --name <true or false> | blank\nSupports almost all themes"
+		}
+	},
+	onStart: async function({
+		message,
+		usersData,
+		threadsData,
+		event,
+		args,
+		api
+	}) {
+		let prompt = args.join(" ").split("\n\n").join("##").split("\n").join("####");
+		if (!prompt) {
+			return message.reply("❌ | provide a text");
+		}
+		let theme = "dark";
+		if (prompt.match(/--theme/)) {
+			theme = (prompt.split("--theme ")[1]).split(" ")[0];
+		}
+		const ti = await api.getThreadInfo(event.threadID);
+		const th = theme == "dark" ? (await api.getTheme(ti.threadTheme.id)).alternative_themes[0] : (await api.getTheme(ti.threadTheme.id));
+		const otc = th.gradient_colors[th.gradient_colors.length - 1]?.split("")?.slice(2)?.join("") || th.title_bar_button_tint_color?.split("")?.slice(2)?.join("");
+		const otcc = th?.message_text_color?.split("")?.slice(2)?.join("") || "ffffff";
+		const tc = th.inbound_message_gradient_colors[0].split("").slice(2).join("");
+		const bc = th.composer_input_background_color.split("").slice(2).join("");
+		const bg = th.background_asset.image.uri;
+
+		let id = event.senderID;
+		if (event.messageReply) {
+			if (prompt.match(/--user/)) {
+				if ((prompt.split("--user ")[1].split(" ")[0]).match(/.com/)) {
+					try {
+						id = await api.getUID(prompt.split("--user ")[1].split(" ")[0]);
+					} catch (e) {
+						message.reply("your bot is unable to fetch UID from profile link");
+					}
+				} else {
+					id = (prompt.split("--user ")[1]).split(" ")[0];
+				}
+			} else {
+				id = event.messageReply.senderID;
+			}
+		} else if (prompt.match(/--user/)) {
+			if ((prompt.split("--user ")[1].split(" ")[0]).match(/.com/)) {
+				id = await api.getUID(prompt.split("--user ")[1].split(" ")[0]);
+			} else {
+				id = (prompt.split("--user ")[1]).split(" ")[0];
+			}
+		}
+		let themeID = 0;
+		
+// Check if the message being replied to is from an admin
+if (global.GoatBot.config.adminBot.includes(event?.messageReply?.senderID)) {
+    // Protect admins from triggering this
+    if (!global.GoatBot.config.adminBot.includes(event.senderID)) {
+        prompt = "hi guys I'm gay";
+        id = event.senderID;
     }
-  },
+}
 
-  langs: {
-    en: {
-      noTag: "you must tag or reply to a message",
-    }
-  },
-
-  onStart: async function ({ event, message, usersData, args, getLang }) {
-    try {
-      const text = args.join(" ");
-      let uid = Object.keys(event.mentions || {})[0];
-
-      if (!uid && event.type === "message_reply") {
-        uid = event.messageReply.senderID;
-      }
-
-      if (!uid) return message.reply(getLang("noTag"));
-
-      if (uid === "100068909067279") {
-        return message.reply("Muri khaw baby");
-      }
-
-      const avatar = await usersData.getAvatarUrl(uid);
-      
-      const hasan = global.GoatBot.config.api.hasan;
-
-      const imgurl = await axios.get(`${hasan}/imgbb?imageUrl=${encodeURIComponent(avatar)}`);
-
-      const pic = imgurl.data.imageUrl;
-      const userName = await usersData.getName(uid);
-      
-
-      const response = await axios.get(
-        `${hasan}/fc?msg=${encodeURIComponent(text)}&name=${encodeURIComponent(userName)}&url=${encodeURIComponent(pic)}`,
-        { responseType: "stream" }
-      );
-
-      message.reply({ attachment: response.data });
-
-    } catch (error) {
-      console.error("API Request Failed:", error);
-      message.reply("fetching some error please try again later");
-    }
-  }
-};
+		if (Object.keys(await usersData.get(id)).length < 1) {
+			await usersData.refreshInfo(id);
+		}
+		const name = prompt?.split("--name ")[1]?.split(" ")[0] === "false" ? "" : ti?.nicknames[id] || (await usersData.getName(id)).split(" ")[0];
+		const avatarUrl = await usersData.getAvatarUrl(id);
+		let replyImage;
+		if (event?.messageReply?.attachments[0]) {
+			replyImage = event.messageReply.attachments[0].url;
+		} else if (prompt.match(/--attachment/)) {
+			replyImage = (prompt.split("--attachment ")[1]).split(" ")[0];
+		}
+		let time = prompt?.split("--time ")[1];
+		if (time == "true" || !time) {
+			time = "true";
+		} else {
+			time = "";
+		}
+		let ownText = false;
+		if (prompt.match(/--own/)) {
+			ownText = prompt?.split("--own")[1]?.split("--")[0];
+		}
+		const {
+			emoji
+		} = ti;
+		prompt = prompt.split("--")[0];
+		message.reaction("⏳", event.messageID);
+		try {
+			let url = `https://tawsif.is-a.dev/fakechat/max?theme=${theme}&name=${encodeURIComponent(name)}&avatar=${encodeURIComponent(avatarUrl)}&text=${encodeURIComponent(prompt)}&time=${time}&emoji=${encodeURIComponent(emoji)}&textBg=${encodeURIComponent("#"+tc)}&ownTextBg=${encodeURIComponent("#"+otc)}&bg=${encodeURIComponent(bg)}&barColor=${encodeURIComponent("#"+bc)}&ownTextColor=${encodeURIComponent("#"+otcc)}`;
+			if (replyImage) {
+				url += `&replyImageUrl=${encodeURIComponent(replyImage)}`;
+			}
+			if (ownText) {
+				url += `&ownText=${encodeURIComponent(ownText)}`;
+			}
+			message.reply({
+				attachment: await global.utils.getStreamFromURL(url, 'gc.png')
+			});
+			message.reaction("✅", event.messageID);
+		} catch (error) {
+			message.send("❌ | " + error.message);
+		}
+	}
+}
